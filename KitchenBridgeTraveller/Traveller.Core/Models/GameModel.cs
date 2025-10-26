0ab3a36e9dde84ba36beb1114c4ec8d2f05dd9e7
Jeff Lefebvre <j.lefebvre@uwinnipeg.ca>
2025-10-25 23:10:09 -0500

    adding service scaffolding

---
+++ KitchenBridgeTraveller/Traveller.Core/Models/GameModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Traveller.Core.Models
+{
+    public class GameModel
+    {
+        public enum Player
+        {
+            North, South, East, West
+        }
+
+        public class GameResult
+        {
+            public int BoardRanking { get; set; }
+            public int MatchPoints { get; set; }
+            public int GameScore { get; set; }
+            public string CardLead { get; set; }
+        }
+
+        public Dictionary<Player, string> PlayerHands { get; set; }
+        public HashSet<GameResult> GameResults { get; set; }
+
+    }
+}
---
+++ KitchenBridgeTraveller/Traveller.Core/Services/FileImportService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Traveller.Core.Models;
+
+namespace Traveller.Core.Services
+{
+    public class FileImportService
+    {
+
+        public HashSet<GameModel> ImportFile(string fileContent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
